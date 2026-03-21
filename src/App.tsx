7427f112012258a64c3f741f470eb4405172d7b9
Lucas <lucas.caetano.reis@hotmail.com>
2026-03-21 15:58:53 -0300

    Importando ícones Lucide para React, finalizando o componente Heading e fazendo a tipagem do Children usando ReactNode.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './components/Heading'
 
 import './styles/theme.css'
 import './styles/global.css'
+import { TimerIcon } from 'lucide-react'
 
 export function App() {
     return (
         <>
-            <Heading attr={123} attr2='String'>Olá mundo!</Heading>
+            <Heading>
+                Olá mundo!
+                <button>
+                    <TimerIcon />
+                </button>
+                </Heading>
             <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente reprehenderit cumque ipsam, perferendis quidem hic consequuntur in dolorum? Libero, eius doloribus omnis saepe non quod eveniet praesentium tempore ducimus laboriosam!
             </p>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,9 @@
 import styles from './Heading.module.css'
 
-export function Heading(props) {
-    return <h1 className={styles.heading}>{props.children}</h1>
+type HeadingProps = {
+    children: React.ReactNode
+}
+
+export function Heading({ children }: HeadingProps) { //Desestruturação das propriedades
+    return <h1 className={styles.heading}>{children}</h1>
 }
