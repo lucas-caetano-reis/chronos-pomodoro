527e9740bd44f0ff8da1c2c81917559d2bbfec98
Lucas <lucas.caetano.reis@hotmail.com>
2026-03-21 20:33:13 -0300

    Criando o formulário da página inicial.

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,27 @@ export function App() {
             </Container>
 
             <Container>
-                    <Heading>Form</Heading>
+                    <form className='form' action=''>
+                        <div className='formRow'>
+                                <label htmlFor='input'>task</label>
+                                <input id='input' type='text' />
+                        </div>
+
+                        <div className='formRow'>
+                                <p>
+                                        Lorem ipsum dolor sit amet.
+                                </p>
+                        </div>
+
+                        <div className='formRow'>
+                                <p>Ciclos</p>
+                                <p>0 0 0 0 0 0 0</p>
+                        </div>
+
+                        <div className='formRow'>
+                                <button>Enviar</button>
+                        </div>
+                    </form>
             </Container>
 
             <Container>
