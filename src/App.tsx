02986029306c232cc6739e828c73e9cdb98a49f9
Lucas <lucas.caetano.reis@hotmail.com>
2026-03-21 17:13:32 -0300

    Criando um React Component para para o Container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
 import './styles/theme.css'
 import './styles/global.css'
 
+import { Container } from './components/Container'
+import { Heading } from './components/Heading'
+
 export function App() {
     return (
         <>
-            <div className="container">
-                <div className="content">
-                    <section>LOGO</section>
-                </div>
-            </div>
+            <Container>
+                    <Heading>LOGO</Heading>
+            </Container>
 
-            <div className="container">
-                <div className="content">
-                    <section>MENU</section>
-                </div>
-            </div>
+            <Container>
+                    <Heading>Menu</Heading>
+            </Container>
 
-            <div className="container">
-                <div className="content">
-                    <section>FORM</section>
-                </div>
-            </div>
+            <Container>
+                    <Heading>Form</Heading>
+            </Container>
 
-            <div className="container">
-                <div className="content">
-                    <section>FOOTER</section>
-                </div>
-            </div>
+            <Container>
+                    <Heading>Footer</Heading>
+            </Container>
         </>
     )
 }
