7e8084f3f8f3ed0a043c9ef717a29adb099f9466
Lucas <lucas.caetano.reis@hotmail.com>
2026-03-21 15:00:09 -0300

    Propriedades (props) e children de componentes no React

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export function App() {
 
     return (
         <>
-            <Heading />
+            <Heading attr={123} attr2='String'>Olá mundo!</Heading>
             <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente reprehenderit cumque ipsam, perferendis quidem hic consequuntur in dolorum? Libero, eius doloribus omnis saepe non quod eveniet praesentium tempore ducimus laboriosam!
             </p>
