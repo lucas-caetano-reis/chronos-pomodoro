485b813239b0dc124014fa7346610fb2a3f6cfc5
Lucas <lucas.caetano.reis@hotmail.com>
2026-03-22 09:41:58 -0300

    Criando um React Component para o rodapé da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Container } from './components/Container'
-import { Heading } from './components/Heading'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
 import { DefaultInput } from './components/DefaultInput'
 import { Cycles } from './components/Cycles'
 import { DefaultButton } from './components/DefaultButton'
+import { PlayCircleIcon } from 'lucide-react'
+import { Footer } from './components/Footer'
 
 import './styles/theme.css'
 import './styles/global.css'
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react'
 
 export function App() {
     return (
@@ -47,13 +47,12 @@ export function App() {
 
                         <div className='formRow'>
                                 <DefaultButton icon={<PlayCircleIcon />} color='green'/>
-                                <DefaultButton icon={<StopCircleIcon />} color='red'/>
                         </div>
                     </form>
             </Container>
 
             <Container>
-                    <Heading>Footer</Heading>
+                    <Footer />
             </Container>
         </>
     )
