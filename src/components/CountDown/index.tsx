80d9a2a9e156b548a5c7d52ded4353fa08970780
Lucas <lucas.caetano.reis@hotmail.com>
2026-03-22 22:05:07 -0300

    useContext + useState

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -3,8 +3,7 @@ import { useTaskContext } from '../../contexts/TaskContext'
 import styles from './styles.module.css'
 
 export function CountDown() {
-    const taskContext = useTaskContext()
-    console.log(taskContext)
+    const {state} = useTaskContext()
 
-    return <div className={styles.container}>00:00</div>
+    return <div className={styles.container}>{state.formattedSecondsRemaining}</div>
 }
