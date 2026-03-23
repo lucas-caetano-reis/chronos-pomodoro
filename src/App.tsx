84b51467b4265d6286460798fb86913f83896e31
Lucas <lucas.caetano.reis@hotmail.com>
2026-03-22 21:41:55 -0300

    Movendo o estado real para dentro do Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Home } from './pages/Home'
 import type { TaskStateModel } from './models/TaskStateModel'
 import { useState } from 'react'
+import { TaskContextProvider } from './contexts/TaskContext'
 
 import './styles/theme.css'
 import './styles/global.css'
-import { TaskContext } from './contexts/TaskContext'
 
 const initialState: TaskStateModel = {
     tasks: [],
@@ -23,8 +23,8 @@ export function App() {
     const [state, setState] = useState(initialState)
 
     return(
-        <TaskContext.Provider value={{ outraCoisa : 321}}>
+        <TaskContextProvider>
             <Home />
-        </TaskContext.Provider>
+        </TaskContextProvider>
     )
 }
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,11 +1,10 @@
-import { useContext } from 'react'
-import { TaskContext } from '../../contexts/TaskContext'
+import { useTaskContext } from '../../contexts/TaskContext'
 
 import styles from './styles.module.css'
 
 export function CountDown() {
-    const taskContext = useContext(TaskContext)
+    const taskContext = useTaskContext()
     console.log(taskContext)
-    
+
     return <div className={styles.container}>00:00</div>
 }
