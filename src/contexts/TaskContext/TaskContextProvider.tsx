3ea4668683e32b94ddca850509c1321988812d64
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-19 12:57:20 -0300

    Revertendo as alterações relacionadas a workers para correção de um bug de contagem de segundos.

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,8 +2,6 @@ import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
-import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
-import { TaskActionTypes } from "./taskActions";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,34 +10,9 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
-  const worker = TimerWorkerManager.getInstance();
-
-  worker.onmessage((e) => {
-    const countDownSeconds = e.data;
-
-    if (countDownSeconds <= 0) {
-      dispatch({
-        type: TaskActionTypes.COMPLETE_TASK,
-      });
-      worker.terminate();
-    } else {
-      dispatch({
-        type: TaskActionTypes.COUNT_DOWN,
-        payload: { secondsRemaining: countDownSeconds },
-      });
-    }
-  });
-
   useEffect(() => {
     console.log(state);
-
-    if (!state.activeTask) {
-      console.log("Worker terminado por falta de activeTask.");
-      worker.terminate();
-    }
-
-    worker.postMessage(state);
-  }, [worker, state]);
+  }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
