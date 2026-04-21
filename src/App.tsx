5dea6006cc0282038161ef5f74d1e95c38312c05
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-20 23:22:49 -0300

    React Router - Criando um Router e um componente ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { Home } from "./pages/Home";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { MessagesContainer } from "./components/MessagesContainer";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
-import { NotFound } from "./pages/NotFound";
+import { MainRouter } from "./routers/MainRouter";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -12,14 +9,7 @@ export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
---
+++ src/routers/MainRouter/index.tsx
@@ -0,0 +1,30 @@
+import { useLocation } from "react-router";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Home } from "../../pages/Home";
+import { AboutPomodoro } from "../../pages/AboutPomodoro";
+import { NotFound } from "../../pages/NotFound";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+}
+
+export function MainRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
+  );
+}
