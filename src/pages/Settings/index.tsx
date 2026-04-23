8b929823ebe24e5f81b0098d010d8616421f76e9
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-22 21:30:51 -0300

    Capturando o onSubmit do formulário com ref dos inputs

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,8 +4,25 @@ import { DefaultButton } from "../../components/DefaultButton";
 import { DefaultInput } from "../../components/DefaultInput";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { useRef } from "react";
 
 export function Settings() {
+  const { state } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+  function handleSaveSettings(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const workTime = workTimeInput.current?.value;
+    const shortBreakTime = shortBreakTimeInput.current?.value;
+    const longBreakTime = longBreakTimeInput.current?.value;
+
+    console.log(workTime, shortBreakTime, longBreakTime);
+  }
+
   return (
     <MainTemplate>
       <Container>
@@ -14,22 +31,31 @@ export function Settings() {
 
       <Container>
         <p style={{ textAlign: "center" }}>
-          Modifique as configurações para tempo de foco, descanso curso e
+          Modifique as configurações para tempo de foco, descanso curto e
           descanso longo.
         </p>
       </Container>
 
       <Container>
-        <form action="" className="form">
-          <div className="formRow">
-            <DefaultInput id="workTime" labelText="Foco" />
-          </div>
-          <div className="formRow">
-            <DefaultInput id="shortBreakTime" labelText="Descanso curto" />
-          </div>
-          <div className="formRow">
-            <DefaultInput id="longBreakTime" labelText="Descanso longo" />
-          </div>
+        <form onSubmit={handleSaveSettings} action="" className="form">
+          <DefaultInput
+              id='workTime'
+              labelText='Foco'
+              ref={workTimeInput}
+              defaultValue={state.config.workTime}
+            />
+          <DefaultInput
+              id='shortBreakTime'
+              labelText='Descanso curto'
+              ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
+            />
+          <DefaultInput
+              id='longBreakTime'
+              labelText='Descanso longo'
+              ref={longBreakTimeInput}
+              defaultValue={state.config.longBreakTime}
+            />
           <div className="formRow">
             <DefaultButton
               icon={<SaveIcon />}
